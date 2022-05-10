c4438c9e58c9ce47e440236c71e6451a0bfd7800
leechao <hello@leechao.cn>
2022-05-10 10:44:35 +0800

    redis对项目进行缓存优化

---
+++ src/main/java/com/leechao/didi/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.leechao.didi.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    /**
+     * 如果自己不创建redistemplate，框架也会创建，但是框架创建的会导致key被序列化
+     * 手写一个redisTemplate，避免key被序列化
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
